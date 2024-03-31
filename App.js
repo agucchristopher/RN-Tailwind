f6e544566368d63dcb6a21a13a60dd7a68a385bb
Agu Christopher <aguchris740@gmail.com>
2024-04-01 00:13:01 +0100

    updates: changed project to simple countdown UI

--- App.js
+++ App.js
@@ -1,21 +1,110 @@
-import { StatusBar } from "expo-status-bar";
-import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
-
-export default function App() {
+import {
+  View,
+  Text,
+  StatusBar,
+  useWindowDimensions,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { FontAwesome } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+const App = () => {
+  let [loaded, error] = useFonts({
+    PBold: require("./assets/Poppins-SemiBold.ttf"),
+  });
+  let [count, setcount] = useState(51);
+  let [activeCount, setactiveCount] = useState(false);
+  let { width, height } = useWindowDimensions();
+  if (!loaded) return <Text>loading..</Text>;
+  let countDown = () => {
+    console.log("Count: ", count);
+    setactiveCount(true);
+    if (count <= 0) {
+      setactiveCount(false);
+      setcount(0);
+      return;
+    } else {
+      setTimeout(() => {
+        setcount((data) => data - 1);
+      }, 1000);
+    }
+  };
   return (
-    <>
-      <View style={{ flex: 1, alignItems: "center" }}>
-        <StatusBar style="light" />
+    <View
+      style={{ backgroundColor: "#303431", flex: 1, justifyContent: "center" }}
+    >
+      <StatusBar backgroundColor={"#303431"} />
+      {/* <Text>App</Text> */}
+      <View
+        style={{
+          height: height * 0.4,
+          borderWidth: 15,
+          borderRadius: width * 0.4,
+          width: width * 0.8,
+          // borderColor: "#3D3F3E",
+          borderLeftColor: "#3D3F3E",
+          borderTopColor: "#3D3F3E",
+          borderBottomColor: "#8CBD83",
+          alignSelf: "center",
+          justifyContent: "center",
+          borderRightColor: "#8CBD83",
+          borderRightWidth: 14,
+          borderBottomRightRadius: -15,
+          borderTopRightRadius: -15,
+          borderCurve: "circular",
+        }}
+      >
+        <Text
+          style={{
+            alignSelf: "center",
+            color: "white",
+            fontFamily: "PBold",
+            fontSize: 35,
+          }}
+        >
+          00:00:{count}
+        </Text>
       </View>
-    </>
+      <View
+        style={{
+          flexDirection: "row",
+          alignSelf: "center",
+          gap: 20,
+          marginTop: 45,
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => countDown()}
+          style={{
+            height: 50,
+            backgroundColor: "#8CBD83",
+            width: 50,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 1000,
+          }}
+        >
+          <FontAwesome
+            color={"black"}
+            size={20}
+            name={!activeCount ? "play" : "pause"}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            backgroundColor: "#3D3F3E",
+            width: 50,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 1000,
+          }}
+        >
+          <FontAwesome color={"white"} size={20} name="repeat" />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
